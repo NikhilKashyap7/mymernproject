d0f0c3937ef0cfbc0e5b23cb88e97d17c141f797
NikhilKashyap7 <168559152+NikhilKashyap7@users.noreply.github.com>
2024-11-14 22:03:47 +0530

    Added log out button

--- clientside/src/modules/reduxpage/Myaction.js
+++ clientside/src/modules/reduxpage/Myaction.js
@@ -1,21 +1,26 @@
-import {createSlice} from '@reduxjs/toolkit';
+import { createSlice } from '@reduxjs/toolkit';
 
-const initialState ={
-    username:"",
-    email:""
+const initialState = {
+    username: "",
+    email: ""
 }
 
 export const counterSlice = createSlice({
-    name:'counter',
+    name: 'counter',
     initialState,
     reducers: {
-        increment: (state,action) => {
+        increment: (state, action) => {
             state.username = action.payload.logindata.fullname || "";
             state.email = action.payload.logindata.email || "";
+        },
+
+        logout: (state) => {
+            state.username = "";
+            state.email = "";
         }
     },
 })
 
-export const {increment} = counterSlice.actions;
+export const { increment, logout } = counterSlice.actions;
 
 export default counterSlice.reducer
--- clientside/src/modules/shares/Myheaderpage.js
+++ clientside/src/modules/shares/Myheaderpage.js
@@ -1,69 +1,88 @@
 import React from 'react'
-import {useSelector} from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
+import { logout } from '../reduxpage/Myaction';
 import { Link } from 'react-router-dom';
 import logo from '../images/zencode.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHouse } from '@fortawesome/free-solid-svg-icons';
 import { faLocationDot } from '@fortawesome/free-solid-svg-icons';
 import { faBook } from '@fortawesome/free-solid-svg-icons';
-import { faRightToBracket } from '@fortawesome/free-solid-svg-icons';
+import LoginIcon from '@mui/icons-material/Login';
+import LogoutIcon from '@mui/icons-material/Logout';
 import AddIcCallIcon from '@mui/icons-material/AddIcCall';
 import PeopleAltIcon from '@mui/icons-material/PeopleAlt';
 import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
-function Myheaderpage() 
-{
-      const vusername = useSelector((state)=> state.counter.username); //Getting username form redux store
+import { toast, ToastContainer } from 'react-toastify';
+function Myheaderpage() {
+  const vusername = useSelector((state) => state.counter.username); //Getting username form redux store
+  const dispatch = useDispatch();
+
+  const handleLogout = () => {
+    if(window.confirm("Are you sure you want to Logout")){
+    dispatch(logout());
+    toast.success("You are successfully Logged Out")
+    }
+  };
+
   return (
     <nav className="navbar navbar-expand-lg bg-body-tertiary sticky-top">
-    <div className="container-fluid ">
-      <Link to='/'><img src={logo} alt='Company logo' className='brand-logo' /></Link>
-      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"></span>
-      </button>
-      <div className="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-          <li className="nav-item">
-            <Link to="/"className="nav-link active underline"  aria-current="page" href="#"><FontAwesomeIcon className='fa-icons' icon={faHouse} />Home</Link>
-          </li>
-          <li className="nav-item">
-            <Link to='/myplacement' className="nav-link active underline" ><FontAwesomeIcon className='fa-icons'icon={faLocationDot} />Placement</Link>
-          </li>
-          <li className="nav-item dropdown">
-                    <Link className="nav-link active  underline"  id="navbarDropdown" role="button"
-                       data-bs-toggle="dropdown" aria-expanded="false">
-                        <FontAwesomeIcon icon={faBook} className='fa-icons'/>
-                        Courses<ArrowDropDownIcon/>
-                    </Link>
-                    <ul className="dropdown-menu "  aria-labelledby="navbarDropdown">
-                        <li><Link className="dropdown-item underline" to="/cybersecurity">Cyber Security</Link></li>
-                        <li><Link className="dropdown-item underline " to="/sap">Sap</Link></li>
-                        <li><Link className="dropdown-item underline" to="/java">Java</Link></li>
-                        <li><Link className="dropdown-item underline" to="/python">Python</Link></li>
-                        <li><Link className="dropdown-item underline" to="/softwaretesting">Software Testing</Link></li>
-                        <li><Link className="dropdown-item underline" to="/graphicdesign">Graphic Designing And Video Editing</Link></li>
-                        <li><Link className="dropdown-item underline" to="/reactjs">React js</Link></li>
-                        <li><Link className="dropdown-item underline" to="/meanstack">Mean Stack</Link></li>
-                        <li><Link className="dropdown-item underline" to="/mern stack">Mern Stack</Link></li>
-                        <li><Link className="dropdown-item underline" to="/digitalmarketing">Digital Marketing </Link></li>
-                        <li><Link className="dropdown-item underline" to="/android">Android</Link></li>
-                        <li><Link className="dropdown-item underline" to="/aws">AWS</Link></li>
-                        <li><Link className="dropdown-item underline" to="/angularjs">Angular js</Link></li>
-                    </ul>
-          </li>
-          <li><Link to='/myloginpage' className="nav-link active me-2 underline"><FontAwesomeIcon icon={faRightToBracket}className='fa-icons' />Login</Link></li>
-          <li><Link to='/ourstudentdata' className="nav-link active me-2 underline"><PeopleAltIcon/>Our Student's</Link></li>
+      <div className="container-fluid ">
+        <Link to='/'><img src={logo} alt='Company logo' className='brand-logo' /></Link>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <Link to="/" className="nav-link active underline" aria-current="page" href="#"><FontAwesomeIcon className='fa-icons' icon={faHouse} />Home</Link>
+            </li>
+            <li className="nav-item">
+              <Link to='/myplacement' className="nav-link active underline" ><FontAwesomeIcon className='fa-icons' icon={faLocationDot} />Placement</Link>
+            </li>
+            <li className="nav-item dropdown">
+              <Link className="nav-link active  underline" id="navbarDropdown" role="button"
+                data-bs-toggle="dropdown" aria-expanded="false">
+                <FontAwesomeIcon icon={faBook} className='fa-icons' />
+                Courses<ArrowDropDownIcon />
+              </Link>
+              <ul className="dropdown-menu " aria-labelledby="navbarDropdown">
+                <li><Link className="dropdown-item underline" to="/cybersecurity">Cyber Security</Link></li>
+                <li><Link className="dropdown-item underline " to="/sap">Sap</Link></li>
+                <li><Link className="dropdown-item underline" to="/java">Java</Link></li>
+                <li><Link className="dropdown-item underline" to="/python">Python</Link></li>
+                <li><Link className="dropdown-item underline" to="/softwaretesting">Software Testing</Link></li>
+                <li><Link className="dropdown-item underline" to="/graphicdesign">Graphic Designing And Video Editing</Link></li>
+                <li><Link className="dropdown-item underline" to="/reactjs">React js</Link></li>
+                <li><Link className="dropdown-item underline" to="/meanstack">Mean Stack</Link></li>
+                <li><Link className="dropdown-item underline" to="/mern stack">Mern Stack</Link></li>
+                <li><Link className="dropdown-item underline" to="/digitalmarketing">Digital Marketing </Link></li>
+                <li><Link className="dropdown-item underline" to="/android">Android</Link></li>
+                <li><Link className="dropdown-item underline" to="/aws">AWS</Link></li>
+                <li><Link className="dropdown-item underline" to="/angularjs">Angular js</Link></li>
+              </ul>
+            </li>
+            <li>
+              {vusername ? (
+                <button onClick={handleLogout} className='nav-link active me-2 underline'><LogoutIcon/>Logout</button>
+              ) : (
+                <Link to='/myloginpage' className="nav-link active me-2 underline "><LoginIcon/>Login</Link>
+              )}
+            </li>
+            <ToastContainer position="top-center" autoClose={2000} />
+
+            <li><Link to='/ourstudentdata' className="nav-link active me-2 underline"><PeopleAltIcon />Our Student's</Link></li>
 
 
-        </ul>
-        <span style={{display:"block", width:"250px"}}> {vusername ? `User: ${vusername}` : ""}</span>
-        <form className="d-flex" role="search">
-        <Link to="/contactus " className="btn nav-link active">
-          <AddIcCallIcon/>Conatct Us
-          </Link>
-        </form>
+          </ul>
+          <span style={{ display: "block", width: "250px" }}> {vusername ? `User: ${vusername}` : ""}</span>
+          <form className="d-flex" role="search">
+            <Link to="/contactus " className="btn nav-link active">
+              <AddIcCallIcon />Conatct Us
+            </Link>
+          </form>
+        </div>
       </div>
-    </div>
-  </nav>
+    </nav>
   )
 }
 
