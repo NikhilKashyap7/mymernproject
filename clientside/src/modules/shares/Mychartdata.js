5a72c13e6f832eba5cf9b156c783798a3aa949e6
NikhilKashyap7 <168559152+NikhilKashyap7@users.noreply.github.com>
2024-11-17 21:46:24 +0530

    updated some components

--- clientside/src/modules/shares/Mychartdata.js
+++ clientside/src/modules/shares/Mychartdata.js
@@ -10,23 +10,25 @@ const CourseChart = ({ CourseCounts = {} }) => {
 
 
   return (
-  <PieChart width={400} height={400}>
-    <Pie
-      data={data}
-      cx={200}
-      cy={200}
-      labelLine={false}
-      label={({ name }) => name}
-      outerRadius={100}
-      fill="#8884d8"
-      dataKey="value"
-    >
-      {data.map((entry, index) => (
-        <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
-      ))}
-    </Pie>
-    <Tooltip />
-  </PieChart>
+    <>
+    <div style={{width:"100%", height:"400"}} />
+    <PieChart width={'100%'} height={'100%'}>
+      <Pie
+        data={data}
+        cx={"50%"}
+        cy={"50%"}
+        labelLine={false}
+        label={({ name }) => name}
+        outerRadius={100}
+        fill="#8884d8"
+        dataKey="value"
+      >
+        {data.map((entry, index) => (
+          <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+        ))}
+      </Pie>
+      <Tooltip />
+    </PieChart></>
 );
 };
 export default CourseChart;
