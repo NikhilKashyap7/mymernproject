4c579a6b206bc1d890ea8d5dd02715858362a9e4
NikhilKashyap7 <168559152+NikhilKashyap7@users.noreply.github.com>
2024-11-17 21:55:12 +0530

    updated some components

--- clientside/src/modules/shares/Mychartdata.js
+++ clientside/src/modules/shares/Mychartdata.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { PieChart, Pie, Cell, Tooltip } from 'recharts';
+import { PieChart, Pie, Cell, Tooltip, ResponsiveContainer } from 'recharts';
 
 
 
@@ -10,25 +10,27 @@ const CourseChart = ({ CourseCounts = {} }) => {
 
 
   return (
-    <>
-    <div style={{width:"100%", height:"400"}} />
-    <PieChart width={'100%'} height={'100%'}>
-      <Pie
-        data={data}
-        cx={"50%"}
-        cy={"50%"}
-        labelLine={false}
-        label={({ name }) => name}
-        outerRadius={100}
-        fill="#8884d8"
-        dataKey="value"
-      >
-        {data.map((entry, index) => (
-          <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
-        ))}
-      </Pie>
-      <Tooltip />
-    </PieChart></>
+    <div style={{ width: '100%', height: '50vh' }}> 
+    <ResponsiveContainer width="100%" height="100%">
+      <PieChart>
+        <Pie
+          data={data}
+          cx="50%" 
+          cy="50%" 
+          labelLine={false}
+          label={({ name }) => name}
+          outerRadius="80%" 
+          fill="#8884d8"
+          dataKey="value"
+        >
+          {data.map((entry, index) => (
+            <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+          ))}
+        </Pie>
+        <Tooltip />
+      </PieChart>
+    </ResponsiveContainer>
+  </div>
 );
 };
 export default CourseChart;
